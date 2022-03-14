09d0d7aa7585a09a9fb1c41af4cf41dca85085ac
khaled-11 <e.khaled.abouseada@gmail.com>
2022-03-14 18:45:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,20 +12,23 @@ function random(n){
     return str;
 }
 
-placeOrder("APQ-3495621S8A1")
+////////// THE SKE ////////////
+placeOrder("APQ-3504275S10A0")
+///////////////////////////////
+
 async function placeOrder(SKU){
     // The payload for the request
-    var payLoad = {"id":3495621,"number":3495621,"name":"#3495621","order_number":3495621,"line_items":[{"id":"3495621","title":"gildan mens t shirt","quantity":1,"price":"45.50","grams":0,"sku":SKU,"name":"gildan mens t shirt - L"}],"billing_address":{"first_name":"john","address1":"1158 am 9.25.19","phone":"2135558976","city":"los angeles","zip":"90013","province":"California","country":"United States","last_name":"smith","address2":"unit 142","name":"john smith","country_code":"US","province_code":"CA"},"shipping_address":{"first_name":"john","address1":"1158 am 9.25.19","phone":"2135558976","city":"los angeles","zip":"90013","province":"California","country":"United States","last_name":"smith","address2":"unit 142","name":"john smith","country_code":"US","province_code":"CA"}}
+    var payLoad = {"id":3504275,"number":1,"name":"#1","order_number":1,"line_items":[{"id":"3504275","title":"beanie","quantity":1,"price":"45.50","grams":0,"sku":SKU,"name":"beanie"}],"billing_address":{"first_name":"john","address1":"1158 bath ave","phone":"3448523355","city":"brooklyn","zip":"11214","province":"New york","country":"United States","last_name":"smith","address2":"unit 142","name":"john smith","country_code":"US","province_code":"NY"},"shipping_address":{"first_name":"john","address1":"1158 bath ave","phone":"3448523355","city":"brooklyn","zip":"11214","province":"New york","country":"United States","last_name":"smith","address2":"unit 142","name":"john smith","country_code":"US","province_code":"NY"}}
     // Stringify and converting into base64 string.
-    let string = JSON.stringify(payLoad.toString('base64'))
+    let string = JSON.stringify(payLoad).toString('base64')
 
     // Authentication variables
     var ran = random(12);
     // Timestamp
     var tp = Math.floor(Date.now()/1000);
-    var app_id = "NTJlY2QwNDI4NTM3NDYyYmJiM2Q3YzQyYjI1YjRjNTQ="
+    var app_id = "MzEyZDVmM2I2ZTcxNDFmYmJlYjQwZDdhODRiNTc4M2U="
     var algorithm = 'SHA256';
-    var key       = 'nkGpoB5DgbrHcBGuWPt+dzfF039Eenwy8ZJrlFQmof8=';
+    var key       = 'NFuEFbSh4S860H7iFaitWepcQ3JE4iRvhBilWVDDFSY=';
     // Text to encrypt
     var hash;
     var text = `${app_id}${tp}${ran}${string}`;
@@ -33,7 +36,7 @@ async function placeOrder(SKU){
     // callback
     hmac.end(text, async function () {
         hash = hmac.read();
-        const url = `https://api.apliiq.com/api/Order`;
+        const url = `https://api.apliiq.com/v1/Order`;
         try {
             const response = await axios.post(url,payLoad,{
                 headers: {
@@ -48,4 +51,6 @@ async function placeOrder(SKU){
             return;
         }
     });
+
+    
 }
